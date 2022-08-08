24f9a97cfe9a93b43e99858d5ca7ac3c8b479136
Minnie Smith <37872572+MinnieSmith@users.noreply.github.com>
2022-08-08 23:14:48 +0800

    Add coffee chatbot ai

--- app/klara.py
+++ app/klara.py
@@ -18,6 +18,9 @@ def main():
     getting_klara_to_help_me_study(user_input[1])
     print("Klara thinks you need to focus on: " + getting_klara_to_help_me_study(user_input[1]))
 
+    coffee = coffee_chatbot(user_input[2])
+    print("coffee chatbot: " + coffee)
+
 def asking_klara(question: str) -> str:
     openai.api_key = os.environ['OPENAI_API_KEY']
     prompt = f"My name is Klara and I'm an observant AF who thrives off the sun's nutrience, ask me a question and I will try to answer {question}"
@@ -41,6 +44,19 @@ def getting_klara_to_help_me_study(topic: str) -> str:
     reply = reply.strip()
 
     return reply
+
+def coffee_chatbot(topic: str) -> str:
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    prompt = f"Cofee chatbot here to help you order coffee{topic}?"
+
+    response = openai.Completion.create(engine='text-davinci-002', prompt=prompt, max_tokens=150)
+
+    # extract the answer from the response and remove whitespaces
+    reply:str = response.choices[0].text
+    reply = reply.strip()
+
+    return reply
+
 
 if __name__ == "__main__":
     main()
